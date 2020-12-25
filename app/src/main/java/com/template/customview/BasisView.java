2970394eb6896040773b02781b898d5a62b3f003
wangyu <wangyu@pdmi.com>
2020-12-25 09:22:03 +0800

    first

---
+++ app/src/main/java/com/template/customview/BasisView.java
@@ -0,0 +1,36 @@
+package com.template.customview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+class BasisView extends View {
+    public BasisView(Context context) {
+        super(context);
+    }
+
+    public BasisView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BasisView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        //设置笔画的基本属性
+        Paint paint=new Paint();
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(50);
+
+        canvas.drawCircle(190,200,150,paint);
+    }
+}
