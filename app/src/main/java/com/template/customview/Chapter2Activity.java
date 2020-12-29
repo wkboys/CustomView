9e12e90132dfe37fcbe92ac33075e13b0019fe6c
wangyu <wangyu@pdmi.com>
2020-12-29 14:48:58 +0800

    finish 2

--- app/src/main/java/com/template/customview/Chapter2Activity.java
+++ app/src/main/java/com/template/customview/Chapter2Activity.java
@@ -1,14 +1,26 @@
 package com.template.customview;
 
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AccelerateInterpolator;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
+import android.view.animation.AnticipateInterpolator;
+import android.view.animation.AnticipateOvershootInterpolator;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.CycleInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.OvershootInterpolator;
 import android.view.animation.RotateAnimation;
 import android.view.animation.ScaleAnimation;
 import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -54,35 +66,104 @@ public class Chapter2Activity extends AppCompatActivity {
 //                translateAnimation.setFillBefore(true);
 //                tv.startAnimation(translateAnimation);
 
-                Animation alpha_Anim=new AlphaAnimation(0.1f,1.0f);
-                Animation scale_Anim=new ScaleAnimation(0.0f,1.4f,0.0f,1.4f,Animation.RELATIVE_TO_PARENT,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
-                Animation rotate_Anim=new RotateAnimation(0,-650,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
-                AnimationSet setAnim=new AnimationSet(true);
-                setAnim.addAnimation(alpha_Anim);
-                setAnim.addAnimation(scale_Anim);
-//                setAnim.addAnimation(rotate_Anim);
-                setAnim.setDuration(3000);
-                setAnim.setFillAfter(true);
-                setAnim.setAnimationListener(new Animation.AnimationListener() {
-                    @Override
-                    public void onAnimationStart(Animation animation) {
+//                Animation alpha_Anim=new AlphaAnimation(0.1f,1.0f);
+//                Animation scale_Anim=new ScaleAnimation(0.0f,1.4f,0.0f,1.4f,Animation.RELATIVE_TO_PARENT,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+//                Animation rotate_Anim=new RotateAnimation(0,-650,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+//                AnimationSet setAnim=new AnimationSet(true);
+//                setAnim.addAnimation(alpha_Anim);
+//                setAnim.addAnimation(scale_Anim);
+////                setAnim.addAnimation(rotate_Anim);
+//                setAnim.setDuration(3000);
+//                setAnim.setFillAfter(true);
+//                setAnim.setAnimationListener(new Animation.AnimationListener() {
+//                    @Override
+//                    public void onAnimationStart(Animation animation) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onAnimationEnd(Animation animation) {
+//                        rotate_Anim.setDuration(3000);
+//                        tv.startAnimation(rotate_Anim);
+//                    }
+//
+//                    @Override
+//                    public void onAnimationRepeat(Animation animation) {
+//
+//                    }
+//                });
+//                setAnim.setInterpolator(new AccelerateDecelerateInterpolator());
+//                tv.startAnimation(setAnim);
 
-                    }
+                Animation rotataAnim=AnimationUtils.loadAnimation(Chapter2Activity.this,R.anim.rotateanim3);
+//                rotataAnim.setInterpolator(new AccelerateDecelerateInterpolator());
+//                rotataAnim.setInterpolator(new AccelerateInterpolator());
+//                rotataAnim.setInterpolator(new DecelerateInterpolator());
+//                rotataAnim.setInterpolator(new LinearInterpolator());
+//                rotataAnim.setInterpolator(new BounceInterpolator());
+//                rotataAnim.setInterpolator(new AnticipateInterpolator());
+//                rotataAnim.setInterpolator(new OvershootInterpolator());
+//                rotataAnim.setInterpolator(new AnticipateOvershootInterpolator());
+                rotataAnim.setInterpolator(new CycleInterpolator(1));
+                tv.startAnimation(rotataAnim);
 
-                    @Override
-                    public void onAnimationEnd(Animation animation) {
-                        rotate_Anim.setDuration(3000);
-                        tv.startAnimation(rotate_Anim);
-                    }
+                ImageView imageView=findViewById(R.id.img);
+                ScaleAnimation scaleAnimation=new ScaleAnimation(1.0f,1.2f,1.0f,1.2f,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+                scaleAnimation.setFillAfter(true);
+                scaleAnimation.setInterpolator(new BounceInterpolator());
+                scaleAnimation.setDuration(6000);
+                imageView.startAnimation(scaleAnimation);
 
-                    @Override
-                    public void onAnimationRepeat(Animation animation) {
-
-                    }
-                });
-                tv.startAnimation(setAnim);
+                 ImageView lodimageView=findViewById(R.id.loading);
+                RotateAnimation rotateAnimation=new RotateAnimation(0,360,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+                rotateAnimation.setRepeatCount(Animation.INFINITE);
+                rotateAnimation.setDuration(2000);
+                rotateAnimation.setInterpolator(new LinearInterpolator());
+                lodimageView.startAnimation(rotateAnimation);
 
             }
         });
+        final Animation animation1 = AnimationUtils.loadAnimation(Chapter2Activity.this,R.anim.scale_alpha_anim);
+        final Animation animation2 = AnimationUtils.loadAnimation(Chapter2Activity.this,R.anim.scale_alpha_anim);
+        final Animation animation3 = AnimationUtils.loadAnimation(Chapter2Activity.this,R.anim.scale_alpha_anim);
+        final Animation animation4 = AnimationUtils.loadAnimation(Chapter2Activity.this,R.anim.scale_alpha_anim);
+
+        final ImageView circle1 = (ImageView)findViewById(R.id.circle1);
+        final ImageView circle2 = (ImageView)findViewById(R.id.circle2);
+        final ImageView circle3 = (ImageView)findViewById(R.id.circle3);
+        final ImageView circle4 = (ImageView)findViewById(R.id.circle4);
+
+        findViewById(R.id.start_can).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                circle1.startAnimation(animation1);
+
+                animation2.setStartOffset(600);
+                circle2.startAnimation(animation2);
+
+                animation3.setStartOffset(1200);
+                circle3.startAnimation(animation3);
+
+                animation4.setStartOffset(1800);
+                circle4.startAnimation(animation4);
+            }
+        });
+        ImageView frame_img = findViewById(R.id.frame_image);
+        AnimationDrawable background = (AnimationDrawable) frame_img.getBackground();
+        background.start();
+
+        ImageView frame_img2 = findViewById(R.id.frame_image2);
+        AnimationDrawable anim=new AnimationDrawable();
+        for (int i = 1; i <= 14; i++) {
+            int id = getResources().getIdentifier("list_icon_gif_playing" + i, "drawable", getPackageName());
+            Drawable drawable = getResources().getDrawable(id);
+            anim.addFrame(drawable, 60);
+        }
+
+        anim.setOneShot(false);
+        frame_img2.setBackgroundDrawable(anim);
+        anim.start();
+
+
     }
 }
